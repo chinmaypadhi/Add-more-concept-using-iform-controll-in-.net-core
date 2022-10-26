d6220d63ad7be969360f8cde01ce057f0aa0527c
chinmay padhi <padhichinmay108@gmail.com>
2022-10-26 23:35:45 +0530

    first commit

---
+++ Iformcollection/Controllers/IformcollectionCon.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Iformcollection.Controllers
+{
+    public class IformcollectionConController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult add(IFormCollection obj)
+        {
+            for (var x= 0; x<obj.Keys.Count-1;x++)
+            {
+                string str = obj["txt1"][x].ToString();
+            }
+            
+
+            return View();
+        }
+    }
+}
---
+++ Iformcollection/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Iformcollection.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
