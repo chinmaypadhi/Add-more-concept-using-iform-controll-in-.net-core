61e2da458d41b5c1424067a61be340c977265294
chinmay padhi <padhichinmay108@gmail.com>
2022-10-27 22:29:41 +0530

    'fdfd'

--- Iformcollection/Controllers/IformcollectionCon.cs
+++ Iformcollection/Controllers/IformcollectionCon.cs
@@ -13,11 +13,26 @@ namespace Iformcollection.Controllers
         {
             return View();
         }
-        public IActionResult add(IFormCollection obj)
+        public class propData
         {
-            for (var x= 0; x<obj.Keys.Count-1;x++)
+            public int studentId { get; set; }
+            public string studentName { get; set; }
+            public string studentCity { get; set; }
+            public string SinglestudentId { get; set; }
+            public string SinglestudentName { get; set; }
+            public string SinglestudentCity { get; set; }           
+        }
+        public IActionResult add(propData propobj,IFormCollection obj)
+        {
+            List<propData> objList = new List<propData>();
+            string[] id = obj["id"].ToString().Split(char.Parse(","));
+            string[] name = obj["name"].ToString().Split(char.Parse(","));
+            string[] city = obj["city"].ToString().Split(char.Parse(","));
+
+            for (var x= 0; x< id.Length; x++)
             {
-                string str = obj["txt1"][x].ToString();
+                propData objProp = new propData() {studentId=Convert.ToInt32(id[x]),studentName= name[x].ToString(),studentCity= city[x].ToString() };               
+                objList.Add(objProp);
             }
             
 
